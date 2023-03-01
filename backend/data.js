fdda9072fe997eb98754275e22ebae96d8eef317
Pankaj Pushkarsingh Rawat <rawatpankaj9876@gmail.com>
2023-03-01 15:41:42 +0530

    20 Seed Sample Users

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Pankaj',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Rawat',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       //_id: 1,
